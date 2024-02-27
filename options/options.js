6327dbd87328fe0720efcd09b88a53e35a1ba718
Crowdbotics <team@crowdbotics.com>
2024-02-27 22:02:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "emurgent-call-46681",
+  url: "https://emurgent-call-46681.botics.co",
+  api: "https://emurgent-call-46681.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
